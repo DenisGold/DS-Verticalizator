e39a01c4f78d9f3ee7b621d46b24ad7a8c897335
Denis Stoliarchuk <dartius@mail.ru>
2016-07-15 22:13:55 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,46 @@
+# jQuery DS VERTICALIZATOR
+
+Can set vertical align for any element
+
+
+
+## Description
+
+Just past into your code $('.vertical').dsVerticalizator(); and all done. For currect display, please add CSS "position: relative" to element
+
+## Browser Compatibility
+
+- [Firefox](http://mzl.la/RNaI) 2.0+
+- [Internet Explorer](http://bit.ly/9fMgIQ) 6+
+- [Safari](http://bit.ly/gMhzVR) 3+
+- [Opera](http://bit.ly/fWJzaC) 10.6+
+- [Chrome](http://bit.ly/ePHvYZ) 8+
+
+
+
+## Installation
+
+<!-- -->
+
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script type="text/javascript" src="path-to-file/jquery.dsVerticalizator.js"></script>
+
+
+
+## Usage
+
+Example code:
+
+    $('.vertical').dsVerticalizator();
+
+
+## Credits
+
+- [WebApple Studio](https://webapple-studio.com)
+
+
+
+## License
+
+The expandable plugin is licensed under the MIT License (LICENSE.txt).
+Copyright (c) 2016 [Denis Stoliarchuk](https://github.com/DenisGold)
---
+++ jquery.dsVerticalizator.js
@@ -0,0 +1,132 @@
+/*
+	* dsSizer 1.1
+	*
+	* Copyright 2016, Denis Stoliarchuk
+	* Licensed under the MIT License (LICENSE.txt).
+	*
+	* Date: Fri Jul 15 21:34:00 2016
+*/
+(function($){
+
+	$.fn.dsSizer = function (method, options){
+	
+		if(!this[method])
+			throw '$.dsSizer => The jQuery method "' + method + '" you called does not exist';
+		
+		var settings = $.extend({
+			absolute      : false,
+			cloned        : false,
+			includeMargin : false,
+			display       : 'block'
+		}, options || {});
+	
+		var self = this.eq(0);
+		var fixIt, restore;
+	
+		if(settings.cloned === true){
+		
+			fixIt = function (){
+				var style = 'position: absolute !important; top: -9999 !important; ';
+				self = self.cloned().attr('style', style).appendTo('body');
+			};
+		
+			restore = function (){ self.remove(); };
+			
+		}else{
+			var tmp   = [],
+				style = '',
+				hidden;
+			
+			fixIt = function (){
+				hidden = self.parents().addBack().filter(':hidden');
+				style += 'visibility: hidden !important; display: ' + settings.display + ' !important;';
+				
+				if(settings.absolute === true) style += 'position: absolute !important;';
+				
+				hidden.each(function(){
+					var self     = $(this),
+						self_style = self.attr('style');
+					
+					tmp.push( self_style );
+					self.attr({style: (self_style)? self_style+';'+style : style});
+				});
+			};
+	
+			restore = function (){
+				hidden.each( function(i){
+					var self  = $( this ),
+						_tmp  = tmp[i];
+					
+					if(_tmp === undefined) self.removeAttr('style');
+					else self.attr('style', _tmp);
+					
+				});
+			};
+		}
+		
+		fixIt();
+		var size = /(outer)/.test(method) ? self[method](settings.includeMargin) : self[method]();
+		restore();
+		
+		return size;
+	}
+
+/* --------------------------------------------------------------------------------------------------------------------------------- */	
+
+/*
+	* jquery.dsVerticalizator.js 1.0
+	*
+	* Copyright 2016, Denis Stoliarchuk
+	* Licensed under the MIT License (LICENSE.txt).
+	*
+	* Date: Fri Jul 15 21:34:00 2016
+*/
+
+	$.fn.dsVerticalizator = function(options) {
+		var settings = $.extend({
+			style:   'top',
+			vertical_offset: 0,
+			parent: null,
+			refresher: 25, 
+			def_winload: false
+		}, options || {});
+		
+		return this.each(function(){
+			var self   = $(this);
+			var timerRef;
+			
+			var resize = function () {
+				var parentHeight = self.parent().dsSizer('height');
+				if(settings.parent && self.parents(settings.parent).length)
+					parentHeight = self.parents(settings.parent).first().dsSizer('height');
+					
+				console.log(self.dsSizer('height'));
+				console.log(self.parent().dsSizer('height'));
+				
+				var centered = (parentHeight / 2) - (self.dsSizer('height') / 2);
+				var centered_with_opts = centered + parseInt(settings.vertical_offset)
+				
+				self.css(settings.style, centered_with_opts);
+				
+				if (settings.complete !== undefined)
+					settings.complete();
+			};
+		
+			$(window).resize(function () {
+				clearTimeout(timerRef);
+				timerRef = setTimeout(resize, settings.refresher);
+			});
+			
+			if (!settings.def_winload) {
+				resize();
+			}
+			
+			$(window).load(function () {
+				resize();
+			});
+		
+		});
+	
+	};
+
+})(jQuery);
